6068dfd4ca15ecd6b500cef18701042b491674e5
Kanhaiya Lal <klal@adobe.com>
2025-04-29 20:17:50 +0530

    update

--- scripts/configs.js
+++ scripts/configs.js
@@ -1,4 +1,4 @@
-import { getHref, getOrigin } from './scripts.js'; //Top of the file Ex: line no : 1
+import { getHref, getOrigin } from './scripts.js'; // Top of the file Ex: line no : 1
 
 const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
 
@@ -10,9 +10,9 @@ const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
  * @returns {string} - environment identifier (dev, stage or prod'.
  */
 export const calcEnvironment = () => {
- const href = getHref();					//add this new line
-const host = getOrigin();				//add this new line
-let environment = 'prod';
+  const href = getHref();					// add this new line
+  const host = getOrigin();				// add this new line
+  let environment = 'prod';
   if (href.includes('.aem.page') || host.includes('staging')) {
     environment = 'stage';
   }
@@ -38,8 +38,8 @@ function buildConfigURL(environment) {
   if (env !== 'prod') {
     fileName = `configs-${env}.json`;
   }
-   const configURL = new URL(`${getOrigin()}/${fileName}`);    //Replace line no 42
- return configURL;
+  const configURL = new URL(`${getOrigin()}/${fileName}`); // Replace line no 42
+  return configURL;
 }
 
 const getConfigForEnvironment = async (environment) => {
